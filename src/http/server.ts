185d8effe3aa94c86665aeedf4301c23bb79a0db
rbsilva1 <rodrigo.belarmino17@gmail.com>
2024-02-07 20:33:09 -0300

    feat: setup fastify and create first routes with fastify

---
+++ src/http/server.ts
@@ -0,0 +1,20 @@
+import fastify from 'fastify';
+import cookie from '@fastify/cookie';
+import { createPoll } from '../routes/create-poll';
+import { getPoll } from '../routes/get-poll';
+import { voteOnPost } from '../routes/vote-on-poll';
+
+const app = fastify();
+
+app.register(cookie, {
+  secret: "polls-app-nlw",
+  hook: 'onRequest'
+});
+
+app.register(createPoll);
+app.register(getPoll);
+app.register(voteOnPost);
+
+app.listen({ port: 3333 }).then(() => {
+  console.log('HTTP server running');
+})
